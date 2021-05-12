73679adadeb39cc70b56dd6ff6d9e8344badb011
yuesh1 <deng550it@gmail.com>
2021-05-12 20:37:47 +0800

    bio-test

--- src/main/java/com/yueshi/netty/bio/TimeServerHandler.java
+++ src/main/java/com/yueshi/netty/bio/TimeServerHandler.java
@@ -1,9 +1,7 @@
 package com.yueshi.netty.bio;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
@@ -30,7 +28,7 @@ public class TimeServerHandler implements Runnable {
     PrintWriter out = null;
     try {
       in = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
-      out = new PrintWriter(this.socket.getOutputStream());
+      out = new PrintWriter(this.socket.getOutputStream(), true);
       String currentTime = null;
       String body = null;
       while (true) {
