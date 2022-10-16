1da56b8870c92310e5755bf843615a7f7a6fc838
yuesh1 <deng550it@gmail.com>
2022-10-16 21:22:36 +0800

    feat: bind target port，increment when port was bound

--- src/main/java/com/yueshi/netty/fakewx/netty/server/NettyServer.java
+++ src/main/java/com/yueshi/netty/fakewx/netty/server/NettyServer.java
@@ -8,19 +8,28 @@ import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.string.StringDecoder;
+import io.netty.util.concurrent.Future;
+import io.netty.util.concurrent.GenericFutureListener;
 
 /**
  * @author: yuesh1 create: 2022-10-16 19:29
  */
 public class NettyServer {
 
+	/**
+	 *
+	 * 启动一个Netty服务端，需要具备以下最小三类： 1、线程模型 2、IO模型 3、连接处理逻辑
+	 */
 	public static void main(String[] args) {
 		ServerBootstrap serverBootstrap = new ServerBootstrap();
-		// boss group 负责创建新连接
+		// boss group 负责创建新连接 accept新连接的线程组
 		// worker 负责读取数据的线程，主要用于读取数据以及业务逻辑处理
 		NioEventLoopGroup bossGroup = new NioEventLoopGroup();
 		NioEventLoopGroup workerGroup = new NioEventLoopGroup();
-		serverBootstrap.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class)
+		serverBootstrap.group(bossGroup, workerGroup)
+				// 指定线程组模型为NIO
+				.channel(NioServerSocketChannel.class)
+				// 定义后续每条连接的数据读写，业务处理逻辑
 				.childHandler(new ChannelInitializer<NioSocketChannel>() {
 					@Override
 					protected void initChannel(NioSocketChannel ch) throws Exception {
@@ -32,7 +41,29 @@ public class NettyServer {
 							}
 						});
 					}
-				}).bind(8000);
+				});
+		bind(serverBootstrap, 22);
+	}
+
+	/**
+	 * 绑定指定端口，如果失败port递增
+	 *
+	 * @param bootstrap
+	 * @param port
+	 */
+	private static void bind(final ServerBootstrap bootstrap, final int port) {
+		bootstrap.bind(port).addListener(new GenericFutureListener<Future<? super Void>>() {
+			@Override
+			public void operationComplete(Future<? super Void> future) throws Exception {
+				if (future.isSuccess()) {
+					System.out.println("端口绑定成功！port:" + port);
+				}
+				else {
+					bind(bootstrap, port + 1);
+					System.out.println("端口绑定失败！port:" + port);
+				}
+			}
+		});
 	}
 
 }
